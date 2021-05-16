32eeacb05a6737d197f3dbf3e8d695f02a349a9e
FrancoAdrianCarrizo <franco.carrizo@bitlogic.io>
2021-05-15 22:30:41 -0300

    exc2

---
+++ exervise-2-horny.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/* 
+El caballo de Mac es más oscuro que el de Smith, pero más rápido y más viejo
+que el de Jack, que es aún más lento que el de Willy, que es más jóven que el de Mac*
+que es más viejo que el de Smith, que es más claro que el de Willy, aunque el de Jack
+es más lento y más oscuro que el de Smith. ¿Cuál es el más viejo, cual el más lento
+y cuál el más claro ?
+
+1.Jack
+2.Willy
+3.Smith
+4. Mac
+
+Claro
+Smith < Jack < Willy < Mac
+
+Viejo
+Smith < Mac < Willy < Jack
+
+Lento
+Jack < Willy < Mac
+*/
+
